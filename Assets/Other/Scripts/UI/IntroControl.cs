83565b674d751f3988fd7a29c3727beb4fe4c9e5
Centil <janbostjancic@gmail.com>
2016-01-08 14:04:35 +0100

    Manjsi updejt

---
+++ Assets/Other/Scripts/UI/IntroControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IntroControl : MonoBehaviour {
+
+	public Transform header = null;
+	public Transform text = null;
+	public float speed = 30;
+	public float distance = 100;
+
+	// Use this for initialization
+	void Start () {
+		if (header != null && text != null) {
+			Text size = text.GetComponent<Text>();
+			text.position = header.position;
+			text.position -= text.up * (size.preferredHeight/2 + distance);
+			Debug.Log(size.preferredHeight/2);
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (header != null && text != null) {
+			header.position += header.up * speed * Time.deltaTime;
+			text.position += header.up * speed * Time.deltaTime;
+		}
+	}
+}
