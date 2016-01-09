0feebff7497ced161d1d57bd9b1a550f5b5442b7
Gasper Kojek <gasper.kojek@gmail.com>
2016-01-09 02:38:33 +0100

    Implemented Main Menu (with mockup menu for options). Implemented switch to PlayerPlotScene on New Game button press.

---
+++ Assets/Other/Scripts/MainMenu/MenuShipRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuShipRotation : MonoBehaviour {
+
+    [Range(0, 20)] public int rotationSpeed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Earth rotation
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+    }
+
+    public void SetRotationSpeed(int speed)
+    {
+        rotationSpeed = speed;
+    }
+}
