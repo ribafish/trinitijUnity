ebbf452b085cd2d9a421ea2d2104209676758a5f
Gasper Kojek <gasper.kojek@gmail.com>
2016-01-09 18:33:20 +0100

    Implemented continuous music between scenes
    Currently just between 00 MainMenu and 01 IntroScene.
    To do this both scenes have to have GameMusic prefab
    To change music use MusicManager prefab

---
+++ Assets/Other/Scripts/Global/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip newMusic;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        GameObject go = GameObject.Find("GameMusic");
+        go.GetComponent<AudioSource>().clip = newMusic;
+        go.GetComponent<AudioSource>().Play();
+    }
+}
