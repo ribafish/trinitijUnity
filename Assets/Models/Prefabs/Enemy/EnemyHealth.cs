8580c9541ace5c0301443616a7a9aac412f40962
Nikolai5 <nikolaj.janko@gmail.com>
2016-01-10 13:20:19 +0100

    Simple enemy collision with bullet

---
+++ Assets/Models/Prefabs/Enemy/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnParticleCollision(GameObject other) {
+		Rigidbody body = other.GetComponent<Rigidbody>();
+		Debug.Log ("Collided");
+	}
+}
