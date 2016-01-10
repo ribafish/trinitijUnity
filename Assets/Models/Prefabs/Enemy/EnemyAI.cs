456775cdd6e0351b3b7b20f765e37b5b590b531b
Nikolai5 <nikolaj.janko@gmail.com>
2016-01-10 20:08:16 +0100

    Ship 2 orientation fixed

---
+++ Assets/Models/Prefabs/Enemy/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	private Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.AddForce (transform.forward*20);
+	}
+}
