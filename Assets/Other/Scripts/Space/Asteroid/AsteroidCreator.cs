42d18c7bec523234ca5cceafe71425ea22136693
Centil <janbostjancic@gmail.com>
2016-01-11 03:29:29 +0100

    Scene 3

---
+++ Assets/Other/Scripts/Space/Asteroid/AsteroidCreator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class AsteroidCreator : MonoBehaviour
+{
+    public float areaSize = 0.0f;
+    public GameObject[] asteroids = null;
+    public int[] asteroidsAmount = null;
+    public Vector2[] asteroidsSizeRange = null;
+
+    public enum RandomChoice {INSIDE_SPHERE, ON_SPEHERE};
+    public RandomChoice randomChoice;
+
+    void Start ()
+    {
+        if (areaSize > 0 && asteroids != null && asteroidsAmount != null)
+        {
+            Vector3 randomPosition = Vector3.zero;
+            float minSize = 0;
+            float maxSize = 0;
+
+            for(int i = 0; i < asteroids.Length; i++)
+            {
+                minSize = asteroidsSizeRange[i].x;
+                maxSize = asteroidsSizeRange[i].y;
+
+                for(int j = 0; j < asteroidsAmount[i]; j++)
+                {
+                    switch (randomChoice)
+                    {
+                        case RandomChoice.INSIDE_SPHERE:
+                            randomPosition = Random.insideUnitSphere * areaSize;
+                            break;
+                        case RandomChoice.ON_SPEHERE:
+                            randomPosition = Random.onUnitSphere * areaSize;
+                            break;
+                    }
+
+                    GameObject asteroid = Instantiate<GameObject>(asteroids[i]);
+                    asteroid.transform.position = transform.position + randomPosition;
+                    asteroid.transform.rotation = Random.rotation;
+                    asteroid.transform.localScale = Vector3.one * Random.Range(minSize, maxSize);
+                }
+            }
+        }
+    }
+
+    //Za lazjo vizualizacijo in izbire
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, areaSize);
+    }
+}
---
+++ Assets/Other/Scripts/Space/Asteroid/AsteroidInitiate.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidInitiate : MonoBehaviour
+{
+    public int speedRange = 0;
+    public int speedRotation = 0;
+
+    Vector3 asteroidMove;
+    float asteroidSpeed;
+    float astroidRoation;
+
+    void Start()
+    {
+        asteroidSpeed = Random.value * Random.Range(0, speedRange);
+        astroidRoation = Random.value * Random.Range(0, speedRotation);
+        int randNum = Random.Range(0,100);
+
+        switch(randNum)
+        {
+            case 1:
+                asteroidMove = new Vector3(1, 0, 0);
+                break;
+            case 2:
+                asteroidMove = new Vector3(0, 1, 0);
+                break;
+            case 3:
+                asteroidMove = new Vector3(0, 0, 1);
+                break;
+            /*case 4:
+                asteroidMove = new Vector3(0, 1, 1);
+                break;
+            case 5:
+                asteroidMove = new Vector3(1, 1, 0);
+                break;
+            case 6:
+                asteroidMove = new Vector3(1, 0, 1);
+                break;
+            case 7:
+                asteroidMove = Vector3.one;
+                break;*/
+            default:
+                asteroidMove = Vector3.zero;
+                break;
+        }
+
+        transform.rotation = Random.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += asteroidMove * asteroidSpeed;
+        float x = transform.rotation.x;
+        float y = transform.rotation.y;
+        float z = transform.rotation.z;
+        float w = transform.rotation.w;
+        x = x + x * astroidRoation;
+        y = y + y * astroidRoation;
+        z = z + z * astroidRoation;
+        transform.rotation.Set(x, y, z, w);
+    }
+}
