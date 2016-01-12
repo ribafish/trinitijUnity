6e8c7218ee33816d58d45d52fe78c379f3e56c91
Nikolai5 <nikolaj.janko@gmail.com>
2016-01-12 23:33:32 +0100

    Enemy

--- Assets/Other/Scripts/Enemy/EnemyAI.cs
+++ Assets/Other/Scripts/Enemy/EnemyAI.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour {
 	public Transform target;
 	public int maxSpeed = 50;
+	public float raydist = 700;
 
 	private float speed = 1f;
 	private Rigidbody rigidbody;
@@ -33,7 +34,6 @@ public class EnemyAI : MonoBehaviour {
 		//transform.rotation
 		//Quaternion targetRot = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
 		RaycastHit hit;
-		bool hitRotation = false;
 
 		//Quaternion targetRot = Quaternion.LookRotation (transform.forward, Vector3.up);
 
@@ -52,15 +52,15 @@ public class EnemyAI : MonoBehaviour {
 			hitvec /= hitColliders.Length;
 		}*/
 
-		float raydist = 120;
 		
-		if (Physics.SphereCast(transform.position, 20, transform.forward, out hit, raydist)) {
-			if (hit.rigidbody.gameObject.tag == "Player") {
+		if (Physics.SphereCast(transform.position, 25, transform.forward, out hit, raydist)) {
+			
+			if (hit.rigidbody != null && hit.rigidbody.gameObject.tag == "Player") {
 				shoot ();
 			} else {
-				Debug.Log ("Hit rotation");
+				Debug.Log (hit.point);
 				//hitvec = (new Vector3 (1000, 1000, 1000) - (transform.position - hit.point));
-				hitvec += (new Vector3 (raydist, raydist, raydist) - (transform.position - hit.point))/5;
+				hitvec += ((new Vector3 (raydist, raydist, raydist) - (transform.position - hit.point))/raydist)*2;
 			}
 			//igralecZivljenja.Hit(1);    //ustreli gralca (trenutno vsak frame, bolje dati timeout)
 		}
@@ -78,20 +78,24 @@ public class EnemyAI : MonoBehaviour {
 			//igralecZivljenja.Hit(1);    //ustreli gralca (trenutno vsak frame, bolje dati timeout)
 		}
 		*/
-		speed = Mathf.Clamp (Mathf.Log10 (hitvec.magnitude), 0.5f, 1f);
+		if(hitvec.sqrMagnitude > 0){
+			rigidbody.velocity = Vector3.Slerp(rigidbody.velocity, transform.forward * 20, Time.deltaTime*2);
+		}
+		speed = Mathf.Clamp (1f / Mathf.Log (hitvec.sqrMagnitude+1), 0.5f, 1f);
+		//Debug.Log (speed);
 
 		//if (!hitRotation) {
 		float distance = Vector3.Distance (target.position, transform.position);
 		//speed = Mathf.Clamp01 (distance / 200f);
-		Vector3 targetVec = (target.position - transform.position).normalized * Mathf.Clamp(1000 - Vector3.Distance(target.position, transform.position), 2, 200);
+		Vector3 targetVec = (target.position - transform.position).normalized * 10; //Mathf.Clamp(1000 - Vector3.Distance(target.position, transform.position), 2, 200);
 
 		Quaternion targetRotTarg = Quaternion.LookRotation (hitvec + targetVec, Vector3.up);
-		hitvec = hitvec / 1.05f;
-		Debug.Log (hitvec.magnitude);
+		hitvec = hitvec / 1.2f;
+		//Debug.Log (hitvec.magnitude);
 		if (distance < 50 || Vector3.Angle((target.position - transform.position), transform.forward) < 3)
 				shoot ();
 
-		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotTarg, Time.deltaTime * speed + 0.2f);
+		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotTarg, Time.deltaTime * speed + 0.3f);
 
 		//}
 
@@ -100,7 +104,7 @@ public class EnemyAI : MonoBehaviour {
 
 	private float shootTime = 0;
 	void shoot(){
-		Debug.Log ("Shoot");
+		//Debug.Log ("Shoot");
 		if (shootTime < 0) {
 			shootTime = 0.5f;
 			foreach (Transform bs in bulletSources)
